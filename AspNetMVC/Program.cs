1deb0463945a3822ebd58c3f6bb1fc7bc9258862
Mikołaj Krukowski <mikolajk12@interia.pl>
2024-09-23 20:14:22 +0200

    Added the seed initializer

--- AspNetMVC/Program.cs
+++ AspNetMVC/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using AspNetMVC.Data;
+using AspNetMVC.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AspNetMVCContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("AspNetMVCContext") ?? throw new InvalidOperationException("Connection string 'AspNetMVCContext' not found.")));
@@ -9,6 +10,13 @@ builder.Services.AddDbContext<AspNetMVCContext>(options =>
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
