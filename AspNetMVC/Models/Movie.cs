6ab2af45fa0a62ffbbb4ca93d58d899a42fe4f9d
Mikołaj Krukowski <mikolajk12@interia.pl>
2024-09-25 19:29:31 +0200

    Fix

--- AspNetMVC/Models/Movie.cs
+++ AspNetMVC/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AspNetMVC.Models
 {
@@ -6,9 +7,12 @@ namespace AspNetMVC.Models
     {
         public int Id { get; set; }
         public string? Title { get; set; }
+
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string? Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
